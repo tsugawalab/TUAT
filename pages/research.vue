3f4737f98c97a7f6213ce20e85ecb8f1aedc57fe
reika0717 <reikahirahara@gmail.com>
2023-01-10 18:53:36 +0900

    researchページの更新

--- pages/research.vue
+++ pages/research.vue
@@ -2,18 +2,41 @@
   <div id="sectionMain" class="mainFrame research">
     <div id="wrapper">
       <h1>{{ $t('research_title') }}</h1>
-      <div class="iframeWrapper">
-        <iframe src="https://onedrive.live.com/embed?cid=F1D3FF272E1AA460&resid=F1D3FF272E1AA460%21335&authkey=AJtY3uNfdtQ6Ma8&em=2" frameborder="0" scrolling="no"></iframe>
+      <div
+        class="researchContent"
+        v-html="$md.render(md)"
+      >
       </div>
-      <h1>Oral talks</h1>
-      <h2>By Hiroshi Tsugawa</h2>
     </div>
   </div>
 </template>
 
 <script>
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
 export default {
   name: 'ResearchPage',
+  async asyncData(ctx) {
+    const { i18n } = ctx.app
+    const localeMap = {
+      ja: 'ja-JP',
+      en: 'en-US',
+    }
+    return await client
+      .getEntries({
+        content_type: 'page',
+        'fields.title': i18n.locale === 'en' ? 'Research' : '研究',
+        locale: localeMap[i18n.locale],
+        limit: 1,
+      })
+      .then((data) => {
+        return { md: data.items[0].fields.body }
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  },
   head() {
     return {
       title: this.$t('member'),
@@ -22,17 +45,18 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-.iframeWrapper
-  width: 100%
-  max-width: 1000px
-  margin: 0 auto
-  padding-bottom: 48.8%
-  position: relative
-  > iframe
+<style lang="sass">
+.researchContent
+  .iframeWrapper
     width: 100%
-    height: 100%
-    position: absolute
-    top: 0
-    left: 0
+    max-width: 1000px
+    margin: 0 auto
+    padding-bottom: 48.8%
+    position: relative
+    iframe
+      width: 100%
+      height: 100%
+      position: absolute
+      top: 0
+      left: 0
 </style>
